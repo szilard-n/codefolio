1f1050673122a1b41a9d3a92b47ccf446908d449
Szilard Nagy <szilardnagy@Szilards-MacBook-Pro.local>
2023-02-15 16:51:35 +0200

    Initial commit

---
+++ src/main/java/com/codefolio/CodfolioApplication.java
@@ -0,0 +1,13 @@
+package com.codefolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodfolioApplication.class, args);
+    }
+
+}
